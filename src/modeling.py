9ea7979e15c65a21e8b0c95cbbdaea0c8b1c1446
CrazyCucumber1337 <isdybnova@edu.hse.ru>
2026-04-26 00:21:19 +0300

    Delete needless line.

--- src/modeling.py
+++ src/modeling.py
@@ -3,7 +3,6 @@ Model training and evaluation for Student Burnout Classification.
 """
 
 import joblib
-import numpy as np
 import pandas as pd
 from sklearn.metrics import (
     accuracy_score,
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -3,7 +3,6 @@ Preprocessing pipeline for Student Burnout Classification.
 """
 
 import pandas as pd
-import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 
